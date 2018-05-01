7cf2e1a76abd10c208296e6f3fec22c1b1ee4229
Steve Hogan <stevenjameshogan@gmail.com>
2018-05-01 14:00:54 -0500

    Delete and Put routes working on Server side

--- server/routes/food.router.js
+++ server/routes/food.router.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    console.log('user', req.user, 'authed?', req.isAuthenticated());
     if(req.isAuthenticated()){
         const queryText = 'SELECT * FROM food_items WHERE user_id = $1;';
         pool.query(queryText, [req.user.id]).then((result) => {
@@ -16,27 +15,50 @@ router.get('/', (req, res) => {
         });
     }   else {
             res.sendStatus(403);
-    }
-    });
+    };
+});
 
 router.post('/', (req, res) => {
-    console.log('user', req.user, 'authed?', req.isAuthenticated());
     if (req.isAuthenticated()) {
         const food = req.body;
-        console.log('food:',food);
         const queryText = `INSERT INTO food_items (user_id, name, quantity, category, location, notes, image_url) 
                          VALUES ($1, $2, $3, $4, $5, $6, $7)`
         pool.query(queryText, [req.user.id, food.name, food.quantity, food.category, food.location, food.notes, food.image_url])
         .then((result) => {
-            console.log('success posting!');
             res.sendStatus(201);
         }).catch((err) => {
             res.sendStatus(500);
-        })
+        });
     } else {
         res.sendStatus(403);
-    }   
-    
+    };   
+});
+
+router.delete('/:id', (req, res) => {
+    if (req.isAuthenticated()) {
+        const queryText = `DELETE FROM food_items WHERE id = $1`;
+        pool.query(queryText, [req.params.id]).then((result) => {
+            res.sendStatus(200);
+        }).catch((err) => {
+            res.sendStatus(500);
+        });
+    };
+});
+
+router.put('/:id', (req, res) => {
+    console.log(req.user, req.body, req.params.id, req.isAuthenticated());
+    const updatedFood = req.body;
+    if (req.isAuthenticated()) {
+        const queryText = `UPDATE food_items SET name = $1, quantity = $2, category = $3, location = $4, notes = $5 WHERE id = $6`;
+        pool.query(queryText, [updatedFood.name, updatedFood.quantity, updatedFood.category, updatedFood.location, updatedFood.notes, req.params.id])
+        .then((result) => {
+            res.sendStatus(201);
+        }).catch((err) => {
+            res.sendStatus(500);
+        });
+    } else {
+        res.sendStatus(403);
+    };   
 })
 
 module.exports = router;
