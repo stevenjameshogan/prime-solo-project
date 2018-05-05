63ef69960e9de06934f8470723c3c1b304ca5e3b
Steve Hogan <stevenjameshogan@gmail.com>
2018-05-05 14:59:39 -0500

    Pluralize and lowe case fixes working

--- server/modules/addFoodIcons.js
+++ server/modules/addFoodIcons.js
@@ -1,6 +1,58 @@
+let pluralize = require('pluralize')
 
 function addFoodIcon(food) {
-    switch(food.name) {
+    switch(pluralize.singular(food.name.toLowerCase())) {
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'apple':
+            return 'apple-1.png';
+            break;
+        case 'asparagus':
+            return 'asparagus.png';
+            break;
+        case 'avocado':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
+        case 'bacon':
+            return 'bacon.png';
+            break;
         case 'bacon':
             return 'bacon.png';
             break;
