577ae79ef93140b660fee37205e0fde0d417a638
Steve Hogan <stevenjameshogan@gmail.com>
2018-05-05 11:43:47 -0500

    Time to branch

--- server/modules/addExpDate.js
+++ server/modules/addExpDate.js
@@ -3,6 +3,13 @@ var moment = require('moment');
 function addExpDate(food) {
     let date = moment();
     console.log(date);
+    let exp_date = moment(date, "MM-DD-YYYY").add(5, 'days')
+    console.log(exp_date);
+    // let difference = moment([2018, 5, 7]).fromNow(true);
+    let difference = date.to(exp_date) 
+    console.log(difference);
+    
+    
     switch(food.category) {
         case 'Meat':
             return moment(date, "MM-DD-YYYY").add(5, 'days');
--- src/redux/reducers/foodReducer.js
+++ src/redux/reducers/foodReducer.js
@@ -1,3 +1,5 @@
+
+
 // Received all food items in a given user's kitchen via action from foodSaga, stores data in state
 const foodReducer = (state=[], action)=>{
     switch(action.type){
